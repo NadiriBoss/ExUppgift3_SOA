ad6b7770811d2e1d37c097b3cc0a346983eb6543
Shinwar <Shinwar@DESKTOP-4J8VLKJ>
2020-11-17 15:01:29 +0100

    Hora

---
+++ ExUppgift3_SOA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExUppgift3_SOA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
