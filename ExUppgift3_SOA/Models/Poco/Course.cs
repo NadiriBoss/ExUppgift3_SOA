c07ee3d33b1c549ab637cd937c52e7c69fec6079
Shinwar <Shinwar@DESKTOP-4J8VLKJ>
2020-11-17 16:03:13 +0100

    poco added

---
+++ ExUppgift3_SOA/Models/Poco/Course.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExUppgift3_SOA.Models
+{
+    public class Course
+    {
+
+    }
+}
---
+++ ExUppgift3_SOA/Models/Poco/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExUppgift3_SOA.Models
+{
+    public class Student
+    {
+        public String Fname { get; set; }
+        public String Lname { get; set; }
+        public String PersNr { get; set; }
+        public String studentUser { get; set; }
+    }
+}
